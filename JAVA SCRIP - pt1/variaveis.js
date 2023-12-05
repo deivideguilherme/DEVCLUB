eb5c45d7448d65c12a43e6a4aea75cb62d440912
deivideguilherme <deivide.jau@gmail.com>
2023-12-04 22:27:53 -0300

    aula de variaveis

---
+++ JAVA SCRIP - pt1/variaveis.js
@@ -0,0 +1,10 @@
+/*
+Variáveis
+    - let ---> Pode alterar o valor da variável a qualquer momento!
+    - const -> Valor constante, não pode ser declarado novamente!
+    - var ---> Descontinuado, não usar!
+*/
+
+let variavel1 = 40
+
+console.log(variavel1)
