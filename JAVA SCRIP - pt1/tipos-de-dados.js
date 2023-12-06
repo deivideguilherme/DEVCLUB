170e103fda76ca2e7c9d22dea85415364428d12b
deivideguilherme <deivide.jau@gmail.com>
2023-12-06 07:30:43 -0300

    Atualizando arquivo tipos-de-dados.js

---
+++ JAVA SCRIP - pt1/tipos-de-dados.js
@@ -0,0 +1,32 @@
+/*
+1. Strings - Textos
+    - "com aspas duplas", 'simples' ou `crase` (crase no javascript, é conhecida como: Template Literals ou Template String)
+
+    Template Literals - essa string aceita que coloquemos uma variável dentro dela junto com um texto por exemplo
+
+
+2. Numbers - Números  
+
+3. Boolean - Boleano (true / false)
+
+4. Object - Objetos
+
+*/
+
+//Exemplo com Template Literals
+const myAge = 32
+
+const myDream = 'Programador'
+
+const myString = `Minha idade é ${myAge}.
+E meu sonho é ser um ${myDream}!
+`
+console.log(myString)
+
+//Exemplo com Numbers
+const number1 = 32
+
+console.log(number1)
+
+//Exemplo com Object
+
