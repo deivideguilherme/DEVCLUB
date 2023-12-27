bdcd50007b315dbd211e8193b7046565c52ba21e
deivideguilherme <deivide.jau@gmail.com>
2023-12-26 22:30:37 -0300

    upgrade aulas javascript + início do projeto conversor

---
+++ JAVA SCRIPT - pt2/alterandoCSSnoJS.js
@@ -0,0 +1,12 @@
+/*
+É possível modificar, manipular qualquer 
+estilo dentro do JavaScript
+*/
+
+const button = document.querySelector(".main-button")
+
+button.style.color = "white"
+button.style.backgroundColor = "blue" //toda propriedade que tiver um 
+                                      //Hífen no meio, remove o Hífen e 
+                                      //deixa a primeira letra do segundo nome Maíscula
+                                      //Ex. background-color = backgroundColor
---
+++ JAVA SCRIPT - pt2/eventos.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#main-input")
+
+function cliqueiNoBotao() {
+    console.log(input.value)
+}
+
+function digiteiNoInput(){
+    console.log (input.value)
+}
