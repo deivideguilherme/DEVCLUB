2b13d842d68dab8c547cbbb3f80ca7931ece84e8
Deivide Guilherme <deivide.jau@gmail.com>
2024-03-18 16:53:33 -0300

    Dando sequencia no módulo de react

--- REACT/first-project/src/App.js
+++ REACT/first-project/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
-import People from './assets/people.svg';
-import Arrow from './assets/arrow.svg'
+import React, { useState } from "react";
+import People from "./assets/people.svg";
+import Arrow from "./assets/arrow.svg";
+import Trash from "./assets/trash.svg";
 
 import {
   Container,
@@ -9,27 +10,63 @@ import {
   ContainerItens,
   InputLabel,
   Input,
-  Button
+  Button,
+  User
 } from "./styles";
 
 //JSX
 const App = () => {
+  //Array de usuários
+  // const users = [];
+
+  //Estado no react / Estado é o mesmo que uma variável
+  //React Hooks => Ferramentas auxiliares
+  const [users, setUsers] = useState([])
+  const [name, setName] = useState()
+  const [age, setAge] = useState()
+
+  function addNewUser() {
+    //Utilizando o Spread Operators (...)
+    setUsers([...users, {id: Math.random(), name, age}])
+  }
+
+  function changeInputName(event) {
+    setName(event.target.value)
+  }
+
+  function changeInputAge(event) {
+    setAge(event.target.value)
+  }
 
   //O return sempre irá retornar HTML
   return (
     //Sempre que utilizo chaves o JSX sabe que vou utilizar elementos Java Script
     <Container>
-      <Image alt="logo-imagem" src={ People } />
+      <Image alt="logo-imagem" src={People} />
       <ContainerItens>
         <H1>Olá</H1>
 
         <InputLabel>Nome</InputLabel>
-        <Input placeholder="Nome" />
+        <Input onChange={changeInputName} placeholder="Nome" />
 
         <InputLabel>Idade</InputLabel>
-        <Input placeholder="Idade" />
+        <Input onChange={changeInputAge} placeholder="Idade" />
 
-        <Button>Cadastrar <img alt="seta" src={ Arrow } /></Button>
+        <Button onClick={addNewUser}>
+          Cadastrar <img alt="seta" src={Arrow} />
+        </Button>
+
+        <ul>
+          {/* Mapeando item por item dentro do array 'users'.
+          Toda vez que eu for iterar itens dentro do React, tenho que definir uma chave unica para o elemento,
+          como foi feito para o <li> = User. */}
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button><img src={Trash} alt="icone-lata-de-lixo" /></button>
+            </User>
+          ))}
+        </ul>
       </ContainerItens>
     </Container>
   );
--- REACT/first-project/src/styles.js
+++ REACT/first-project/src/styles.js
@@ -1,96 +1,131 @@
-import styled from 'styled-components'
-import Background from './assets/background1.svg'
+import styled from "styled-components";
+import Background from "./assets/background1.svg";
 
 export const Container = styled.div`
-    background: url('${Background}');
-    background-size: cover;
+  background: url("${Background}");
+  background-size: cover;
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
 
-    height: 100vh;
-`
+  height: 100vh;
+`;
 
 export const Image = styled.img`
-    margin-top: 30px;
-`
+  margin-top: 30px;
+`;
 
 export const ContainerItens = styled.div`
-    background: linear-gradient(
-        157.44deg, 
-        rgba(255, 255, 255, 0.6) 0.84%,
-        rgba(255, 255, 255, 0.6) 0.85%,
-        rgba(255, 255, 255, 0.15) 100%
-    );
+  background: linear-gradient(
+    157.44deg,
+    rgba(255, 255, 255, 0.6) 0.84%,
+    rgba(255, 255, 255, 0.6) 0.85%,
+    rgba(255, 255, 255, 0.15) 100%
+  );
 
-    border-radius: 61px 61px 0 0;
-    padding: 50px 36px;
+  border-radius: 61px 61px 0 0;
+  padding: 50px 36px;
 
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    height: 100vh;
+  height: 100vh;
 `;
 
 export const H1 = styled.h1`
-    margin-bottom: 80px;
-    font-size: 34px;
-    font-weight: bold;
-    line-height: 40px;
-    text-align: center;
-    color: #FFF;
-`
+  margin-bottom: 80px;
+  font-size: 34px;
+  font-weight: bold;
+  line-height: 40px;
+  text-align: center;
+  color: #fff;
+`;
 
 export const InputLabel = styled.p`
-    letter-spacing: -0.408px;
-    font-size: 18px;
-    font-weight: bold;
-    line-height: 22px;
-    color: #EEE;
-    margin-left: 25px;
-`
+  letter-spacing: -0.408px;
+  font-size: 18px;
+  font-weight: bold;
+  line-height: 22px;
+  color: #eee;
+  margin-left: 25px;
+`;
 
 export const Input = styled.input`
-    background: rgba(255, 255, 255, 0.25);
-    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 14px;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 14px;
 
-    width: 342px;
-    height: 58px;
-    padding-left: 25px;
-    margin-bottom: 34px;
+  width: 342px;
+  height: 58px;
+  padding-left: 25px;
+  margin-bottom: 34px;
 
-    border: none;
-    outline: none;
+  border: none;
+  outline: none;
 
-    //Texto interno
-    color: #FFF;
-    font-size: 20px;
-    line-height: 28px;
-
-`
+  //Texto interno
+  color: #fff;
+  font-size: 20px;
+  line-height: 28px;
+`;
 
 export const Button = styled.button`
-    width: 342px;
-    height: 74px;
-    background: rgba(0, 0, 0, 0.8);
-    border-radius: 14px;
+  width: 342px;
+  height: 74px;
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: 14px;
 
-    border: none;
+  border: none;
 
-    //Texto interno
-    font-weight: bold;
-    font-size: 17px;
+  //Texto interno
+  font-weight: bold;
+  font-size: 17px;
+  line-height: 28px;
+  color: #fff;
+  cursor: pointer;
+
+  //Imagem da seta
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+
+  &:hover {
+    opacity: 0.8;
+  }
+  &:active {
+    opacity: 0.5;
+  }
+`;
+
+export const User = styled.li`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 20px;
+
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 14px;
+  width: 342px;
+  height: 58px;
+
+  border: none;
+  outline: none;
+
+  p {
+    color: #fff;
+    font-size: 20px;
     line-height: 28px;
-    color: #FFF;
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  button {
+    background: none;
+    border: none;
     cursor: pointer;
-
-    //Imagem da seta
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 20px;
-`
-
+  }
+`;
