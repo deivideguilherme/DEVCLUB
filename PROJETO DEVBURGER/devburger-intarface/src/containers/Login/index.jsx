dffccafb49ee598e551a87ca64ec196a3b418020
Deivide Guilherme <deivide.jau@gmail.com>
2024-05-15 16:54:52 -0300

    Estilizando tela e login do projeto

--- PROJETO DEVBURGER/devburger-intarface/src/containers/Login/index.jsx
+++ PROJETO DEVBURGER/devburger-intarface/src/containers/Login/index.jsx
@@ -1,7 +1,34 @@
+import { Button, Container, Form, InputContainer, LeftContainer, RightContainer, Title } from './styles';
+import Logo from '../../assets/logo.svg';
+
 export function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt='logo-devburger' />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>Dev Burguer!</span>
+          <br />
+          Acesse com seu <span>Login e senha.</span>
+        </Title>
+        <Form>
+          <InputContainer>
+            <label>E-mail</label>
+            <input type="email" />
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" />
+          </InputContainer>
+          <Button>Entrar</Button>
+        </Form>
+        <p>
+          Não possui conta? <a>Clique aqui.</a>
+        </p>
+      </RightContainer>
+    </Container>
   );
 }
--- PROJETO DEVBURGER/devburger-intarface/src/main.jsx
+++ PROJETO DEVBURGER/devburger-intarface/src/main.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Login } from "./containers/Login";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GlobalStyles from './styles/globalStyles';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import { Login } from './containers/Login';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Login />
+    <GlobalStyles />
   </React.StrictMode>
 );
