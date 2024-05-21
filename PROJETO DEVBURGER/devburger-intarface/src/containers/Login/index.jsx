b9e824ec5101887e6e14220d638dd2f821d74748
Deivide Guilherme <deivide.jau@gmail.com>
2024-05-21 09:03:02 -0300

    Finalizado módulo de interface pt.1

--- PROJETO DEVBURGER/devburger-intarface/src/containers/Login/index.jsx
+++ PROJETO DEVBURGER/devburger-intarface/src/containers/Login/index.jsx
@@ -1,7 +1,45 @@
-import { Button, Container, Form, InputContainer, LeftContainer, RightContainer, Title } from './styles';
+//Formulário
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { api } from "../../services/api";
+
+import { Container, Form, InputContainer, LeftContainer, RightContainer, Title } from './styles';
+import { Button } from '../../components/Button';
 import Logo from '../../assets/logo.svg';
 
 export function Login() {
+
+  //Validações do formulário
+  const schema = yup
+    .object({
+      email: yup
+        .string()
+        .email('Digite um e-mail válido')
+        .required('O e-mail é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter ao menos 6 caracteres')
+        .required('Digite uma senha'),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+  const onSubmit = async (data) => {
+    const response = await api.post('/session', {
+      email: data.email,
+      password: data.password,
+    });
+
+    console.log(response);
+  }
+
   return (
     <Container>
       <LeftContainer>
@@ -13,17 +51,22 @@ export function Login() {
           <br />
           Acesse com seu <span>Login e senha.</span>
         </Title>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <InputContainer>
             <label>E-mail</label>
-            <input type="email" />
+            <input type="email" {...register('email')} />
+            {/* Elvis Operator '?' */}
+            <p>{errors?.email?.message}</p>
           </InputContainer>
 
           <InputContainer>
             <label>Senha</label>
-            <input type="password" />
+            <input type="password" {...register('password')} />
+            {/* Elvis Operator '?' */}
+            <p>{errors?.password?.message}</p>
           </InputContainer>
-          <Button>Entrar</Button>
+
+          <Button type="submit">Entrar</Button>
         </Form>
         <p>
           Não possui conta? <a>Clique aqui.</a>
--- PROJETO DEVBURGER/devburger-intarface/src/containers/Login/styles.js
+++ PROJETO DEVBURGER/devburger-intarface/src/containers/Login/styles.js
@@ -3,94 +3,99 @@ import BackgroundLogin from '../../assets/background-login.svg';
 import Background from '../../assets/background.svg';
 
 export const Container = styled.div`
-    display: flex;
-    height: 100vh;
-    width: 100vw;
+  display: flex;
+  height: 100vh;
+  width: 100vw;
 `;
 
 export const LeftContainer = styled.div`
-    background: url('${BackgroundLogin}');
-    background-size: cover;
-    background-position: center;
-    
-    height: 100%;
-    width: 100%;
-    max-width: 50%;
+  background: url('${BackgroundLogin}');
+  background-size: cover;
+  background-position: center;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 100%;
+  width: 100%;
+  max-width: 50%;
 
-    img {
-        width: 80%;
-    }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 80%;
+  }
 `;
 
 export const RightContainer = styled.div`
-    background: url('${Background}');
-    background-color: #1E1E1E;
+  background: url('${Background}');
+  background-color: #1e1e1e;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
-    height: 100%;
-    width: 100%;
-    max-width: 50%;
+  height: 100%;
+  width: 100%;
+  max-width: 50%;
 
-    p {
-      color: #fff;
-      font-size: 18px;
-      font-weight: 800;
+  p {
+    color: #fff;
+    font-size: 18px;
+    font-weight: 800;
 
-      a {
-        text-decoration: underline;
-      }
-
+    a {
+      text-decoration: underline;
     }
+  }
 `;
 
 export const Title = styled.h2`
-  font-family: "Road Rage", sans-serif;
+  font-family: 'Road Rage', sans-serif;
   font-size: 40px;
-  color: #FFF; 
+  text-align: center;
+  color: #fff;
 
   span {
-    color: #9758A6;
+    font-family: 'Road Rage', sans-serif;
+    color: #9758a6;
   }
 `;
 
 export const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    padding: 20px;
-    width: 100%;
-    max-width: 400px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+  width: 100%;
+  max-width: 400px;
 `;
 
 export const InputContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  width: 100%;
+
+  input {
     width: 100%;
+    border: none;
+    height: 52px;
+    border-radius: 5px;
+    padding: 0 16px;
+  }
 
-    input {
-      width: 100%;
-      border: none;
-      height: 52px;
-      border-radius: 5px;
-      padding: 0 16px;
-    }
+  label {
+    font-size: 18px;
+    font-weight: 600;
+    color: #fff;
+  }
 
-    label {
-      font-size: 18px;
-      font-weight: 600;
-      color: #FFF;
-    }
-`;
-
-export const Button = styled.button`
-    
+  p {
+    font-size: 14px;
+    line-height: 80%;
+    color: #cf3057;
+    font-weight: 600;
+    height: 10px;
+  }
 `;
